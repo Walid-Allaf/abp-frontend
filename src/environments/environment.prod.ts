025a0c985810b53e3986a21e39c3fd2ba5946d0a
Walid-Allaf <hammoda3165@gmail.com>
2024-06-25 03:10:55 +0300

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'abp_example',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44339/',
+    redirectUri: baseUrl,
+    clientId: 'abp_example_App',
+    responseType: 'code',
+    scope: 'offline_access abp_example',
+    requireHttps: true
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44339',
+      rootNamespace: 'abp_example',
+    },
+  },
+} as Environment;
